5c3e087cf4f10a8e110e15b7dee8a2da9c89866b
TijnDN <tijnden@gmail.com>
2026-04-15 11:26:50 +0200

    Running

--- Assets/C# scripts/NewMovement.cs
+++ Assets/C# scripts/NewMovement.cs
@@ -3,17 +3,48 @@ using UnityEngine.InputSystem;
 
 public class NewMovement : MonoBehaviour
 {
-    public float speed = 5f;
     private CharacterController cc;
     private Vector2 moveInput;
 
-    void Start() => cc = GetComponent<CharacterController>();
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 8f;
+
+    [Header("Stamina")]
+    [SerializeField] float maxStamina = 100f;
+    [SerializeField] float regenRate = 20f;
+    [SerializeField] float drainRate = 30f;
+
+    private float currentStamina;
+    private bool isRunning;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        currentStamina = maxStamina;
+    }
 
     void Update()
     {
+        Stamina();
+
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        move = transform.TransformDirection(move) * speed;
+        move = transform.TransformDirection(move) * currentSpeed;
+
         cc.Move(move * Time.deltaTime);
+    }
+
+    void Stamina()
+    {
+        isRunning = Input.GetKey(KeyCode.LeftShift) && currentStamina > 0f;
+
+        if (isRunning)
+            currentStamina -= drainRate * Time.deltaTime;
+        else if (currentStamina < maxStamina)
+            currentStamina += regenRate * Time.deltaTime;
+
+        currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
     }
 
     public void OnMove(InputValue value) => moveInput = value.Get<Vector2>();
