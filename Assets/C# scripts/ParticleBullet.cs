98cd255e8dd5adc6bf51c15020aec3aaedb31cb5
TamasHertogs <118199296+TamasssH@users.noreply.github.com>
2026-05-29 13:25:22 +0200

    New environment models
     New environment models + laser bullet particle and script.

---
+++ Assets/C# scripts/ParticleBullet.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBullet : MonoBehaviour
+{
+    public ParticleSystem ParticleSys;
+    public float fireRate = 0.2f;
+    public int damage = 20;
+    public GameObject spark;
+
+    private float nextFireTime = 0f;
+
+    List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+
+    private void Start()
+    {
+        ParticleSys = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            ParticleSys.Play();
+        }
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Hit");
+        //int events = ParticleSys.GetCollisionEvents(other, colEvents);
+        //Debug.Log("Hit");
+        //for (int i = 0; i < events; i++) 
+        //{
+        //    Instantiate(spark, colEvents[i].intersection, Quaternion.LookRotation(colEvents[i].normal));
+        //}
+    }
+}
