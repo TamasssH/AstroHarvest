502606101a76b3a0bf0cb8c136869874565a5feb
TijnDN <tijnden@gmail.com>
2026-06-24 15:31:25 +0200

    sound

--- Assets/Materials/C# scripts/ParticleBullet.cs
+++ Assets/Materials/C# scripts/ParticleBullet.cs
@@ -6,11 +6,21 @@ public class ParticleBullet : MonoBehaviour
     public float fireRate = 0.2f;
     public int damage = 20;
 
+    
+    public AudioClip shootSound;
+    private AudioSource audioSource;
+
     private float nextFireTime = 0f;
 
     void Start()
     {
-        if (ParticleSys == null) ParticleSys = GetComponent<ParticleSystem>();
+        if (ParticleSys == null)
+            ParticleSys = GetComponent<ParticleSystem>();
+
+        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     void Update()
@@ -18,13 +28,19 @@ public class ParticleBullet : MonoBehaviour
         if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
             nextFireTime = Time.time + fireRate;
-            if (ParticleSys) ParticleSys.Play();
+
+            if (ParticleSys)
+                ParticleSys.Play();
+
+            if (shootSound != null)
+                audioSource.PlayOneShot(shootSound);
         }
     }
 
     public void OnParticleCollision(GameObject other)
     {
         Health health = other.GetComponent<Health>();
+
         if (health != null)
         {
             health.TakeDamage(damage);
